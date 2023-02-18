b53eb22df939648772e9ac5a1ae506a5bdd60f7f
Jason Wieseler <jasonwieseler@gmail.com>
2023-02-18 10:05:09 -0500

    Added all files and desc.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Control-charts
-Collection of spc charts in python by R. Salazar
+Modified collection of spc charts in Python
+These are taken from the excellent Medium article below. The hope it to beautify the charting of these and perhaps add some implemention examples in a health care setting. 
+
+
+https://towardsdatascience.com/quality-control-charts-guide-for-python-9bb1c859c051
+Quality Control Charts with Python
+by
+Roberto Salazar
+
+May 11, 2020
---
+++ x-bar chart and R chart.py
@@ -0,0 +1,73 @@
+# Import required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics
+
+# Set random seed
+np.random.seed(42)
+
+# Create dummy data
+x = np.array([list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=17, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5)),
+        list(np.random.normal(loc=10, scale=2, size=5))])
+        
+# Define list variable for groups means
+x_bar = []
+
+# Define list variable for groups ranges
+r = [] 
+
+# Get and append groups means and ranges
+for group in x:
+    x_bar.append(group.mean())
+    r.append(group.max() - group.min())
+    
+# Plot x-bar and R charts
+fig, axs = plt.subplots(2, figsize=(15,15))
+
+# x-bar chart
+axs[0].plot(x_bar, linestyle='-', marker='o', color='black')
+axs[0].axhline((statistics.mean(x_bar)+0.577*statistics.mean(r)), color='red', linestyle='dashed')
+axs[0].axhline((statistics.mean(x_bar)-0.577*statistics.mean(r)), color='red', linestyle='dashed')
+axs[0].axhline((statistics.mean(x_bar)), color='blue')
+axs[0].set_title('x-bar Chart')
+axs[0].set(xlabel='Group', ylabel='Mean')
+
+# R chart
+axs[1].plot(r, linestyle='-', marker='o', color='black')
+axs[1].axhline((2.574*statistics.mean(r)), color='red', linestyle='dashed')
+axs[1].axhline((0*statistics.mean(r)), color='red', linestyle='dashed')
+axs[1].axhline((statistics.mean(r)), color='blue')
+axs[1].set_ylim(bottom=0)
+axs[1].set_title('R Chart')
+axs[1].set(xlabel='Group', ylabel='Range')
+
+# Validate points out of control limits for x-bar chart
+i = 0
+control = True
+for group in x_bar:
+    if group > statistics.mean(x_bar)+0.577*statistics.mean(r) or group < statistics.mean(x_bar)-0.577*statistics.mean(r):
+        print('Group', i, 'out of mean control limits!')
+        control = False
+    i += 1
+if control == True:
+    print('All points within control limits.')
+    
+# Validate points out of control limits for R chart
+i = 0
+control = True
+for group in r:
+    if group > 2.574*statistics.mean(r):
+        print('Group', i, 'out of range cotrol limits!')
+        control = False
+    i += 1
+if control == True:
+    print('All points within control limits.')
